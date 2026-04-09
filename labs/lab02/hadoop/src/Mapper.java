a8f9d67c51213c376af75da481adbc5dbfa85459
JimmyChin946 <Judes946@gmail.com>
2026-04-09 13:42:40 -0700

    mapper

---
+++ labs/lab02/hadoop/src/Mapper.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.Mapper.*;
+
+public class Mapper extends Mapper<> {
+	@Override
+	public void map(LongWritable key, Text value, Context context) 
+		throws IOException, InterruptedException {
+
+		// The string in one line of the input file
+		String fullLine = value.toString().trim();
+
+		// Make sure there are only 5 elements in a line
+		// ID, date, time, storeID, customerID
+		String[] tokens = fullLine.split(" ");
+		if (tokens.length != 5) {
+			return;
+		}
+
+		context.write(
+				new Text(tokens[1]),	// The date as yyyy/mm/dd
+				new NullWritable());	// We don't really care...
+	}
+}
