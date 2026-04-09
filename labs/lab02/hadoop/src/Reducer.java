c8e42c3ed9ef87c96a92b086d465f873c6d7985e
JimmyChin946 <Judes946@gmail.com>
2026-04-09 13:49:43 -0700

    reducer

---
+++ labs/lab02/hadoop/src/Reducer.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.Reducer.*;
+
+
+public class Reducer extends Reducer<Text, NullWritable, Text, IntWritable> {
+	@Override
+	public void reduce(Text date, Iterable<NullWritable> nulls, Context context) 
+		throws IOException, InterruptedException {
+	
+		// Since we are dealing with an iterable, we need to manually 
+		// iterate over the entire list to find the length
+		int count = 0;
+		for (NullWritable n : nulls) {
+			count++;
+		}
+
+		context.write(date, new IntWritable());
+	}
+}
+
