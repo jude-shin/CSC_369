530e69efda2016274011211245f434711cb613da
JimmyChin946 <Judes946@gmail.com>
2026-04-16 18:55:35 -0700

    copy from local, except I think put is the better command

--- copyLab01FilesHadoop.sh
+++ copyLab01FilesHadoop.sh
@@ -4,9 +4,12 @@ LOCAL_DIR="/home/jshin53/CSC_369/labs/lab01/app/output"
 
 for name in customer lineItem product sale store
 do
-    # Erase the directory for simplicity
+		echo "Erasing previous hdfs directory"	
     hadoop fs -rm -r /user/jshin53/input/$name
+	
+		echo "Creating hdfs directory"
+		hadoop fs -mkdir /user/jshin53/input/$name
 
-    # Copy over the file
-    hadoop fs -copyFromLocal $LOCAL_DIR/$name /user/jshin53/input/$name
+		echo "Putting local file in new directory"
+		hadoop fs -put $LOCAL_DIR/$name /user/jshin53/input/$name/$name
 done
