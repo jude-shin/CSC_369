a29eb641b51a3af62a32e81be4a96feb34569a8a
JimmyChin946 <Judes946@gmail.com>
2026-04-17 18:25:20 -0700

    mapper that parses the information

--- assignments/asgn02/hadoop/MyMapper.java
+++ assignments/asgn02/hadoop/MyMapper.java
@@ -16,19 +16,22 @@ public class MyMapper
 		// Split the values by the commas
 		String[] tokens = fullLine.split(",");
 	
-		// Sales file: ID, date, time, storeID, customerID
-		if (tokens.length != 5) {
+		// Input File: name, id, grade, course
+		if (tokens.length != 4) {
 			return;
 		}
 		
 		// Extract the data from the line
-		String id = tokens[0].trim();
-		String date = tokens[1].trim();
-		String time = tokens[2].trim();
+		String name = tokens[0].trim();
+		String id = tokens[1].trim();
+		String grade = tokens[2].trim();
+		String course = tokens[3].trim();
 	
-		// The key will be our custom key which will be sorted by date, then time
-		// Nothing special needs to happen with the value, it is just the date of 
-		// the sale and the id of that sale.
-		context.write(new DateTimePair(date, time), new Text(time + " " + id));
+		// The key holds the name (and the id), and the grade for sorting
+		Writable k = new StudentTripple(name, Integer.parseInt(id), grade);
+		// The value holds the grade (again) and the course name
+		Writable v = new GradePair(grade, course);
+	
+		context.write(k, v);
 	}
 }
