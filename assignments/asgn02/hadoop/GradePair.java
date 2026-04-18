9de25370bf02df6d6ef35f77e1c6dca33165955b
JimmyChin946 <Judes946@gmail.com>
2026-04-17 18:53:29 -0700

    there needs to be a space

--- assignments/asgn02/hadoop/GradePair.java
+++ assignments/asgn02/hadoop/GradePair.java
@@ -49,6 +49,6 @@ public class GradePair implements Writable {
 	// Hopefully this makes things easier
 	@Override
 	public String toString() {
-		return "(" + grade.toString() + course.toString() + ")";
+		return "(" + grade.toString() + " " + course.toString() + ")";
 	}
 }
