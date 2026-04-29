607a2a91e4180bedb197b31a9291c542fbb14475
JimmyChin946 <Judes946@gmail.com>
2026-04-29 15:37:17 -0700

    one single reduce

--- labs/lab04/hadoop/MyReducer.java
+++ labs/lab04/hadoop/MyReducer.java
@@ -2,28 +2,53 @@ import java.io.*;
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.*;
 import org.apache.hadoop.mapreduce.Reducer.*;
+import java.util.TreeSet;
 
-
-public class MyReducer 
-	extends Reducer<DateTimePair, Text, Text, Text> {
+public class MyReducer extends Reducer<NullWritable, Text, NullWritable, Text> {
+	private int n = MyDriver.DEFAULT_N;
+	private SortedSet<Record> top = new TreeSet<>();
 
 	@Override
-	public void reduce(DateTimePair dateTime, Iterable<Text> timeIdPairs, Context context) 
+	public void reduce(NullWritable key, Iterable<Text> values, Context context) 
 		throws IOException, InterruptedException {
-	
-		// The comma separated values that hold space separated (date id) pairs.
-		String value = ""; 
+		// For every line that is given to one reduce node
+		for (Text value : values) {
+			// The full line from the input file
+			String line = value.toString().trim();
 
-		// Concatinate all of our items (will be in sorted order) separating by ", "
-		for (Text p : timeIdPairs) {
-			value = value + p.toString() + ", ";
+			// Split based on a comma
+			String[] tokens = line.split(",");
+
+			// Parse out the id, name, and the price
+			int id = Integer.parseInt(tokens[0].trim());
+			String name = tokens[1].trim();
+			double price = Double.parseDouble(tokens[2].trim());
+
+			// Add the element to the sorted running set
+			top.add(new Record(id, name, price));
+
+			// Keep only top n
+			if (top.size() > n) {
+				top.remove(top.last());
+			}
 		}
+	
+		// Finally, write the global top n records as the output
+		for(Record r : top){
+			// Will be in the same form as the input ("id, name, price")
+			context.write(NullWritable.get(), new Text(r.toString()));
+		}
+	}
 
-		// Clean up the leftover ", " at the end of our value string (2 characters)
-		value = value.substring(0, value.length()-2);
+	// Run before the reduce stage
+	@Override
+	protected void setup(Context context) throws IOException, InterruptedException {
+		// Either sets up the global variable from the given context, or the default
+		this.n = context.getConfiguration().getInt("N", DEFAULT_N); 
 
-		// Write the only the date as the key, and our concatinated string
-		// as the value.
-		context.write(dateTime.getDate(), new Text(value));
+		// If for some reason the top is not initalized, initalize it to an empty set
+		if (this.top == null) {
+			this.top = new TreeSet<Record>();
+		}
 	}
 }
