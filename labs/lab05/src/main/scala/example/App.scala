82236740625248ae49b869b81cf3229870ee42de
JimmyChin946 <Judes946@gmail.com>
2026-05-07 12:54:19 -0700

    make a funtion

--- labs/lab05/src/main/scala/example/App.scala
+++ labs/lab05/src/main/scala/example/App.scala
@@ -1,6 +1,15 @@
 package example
 object App { // Object means that everything in this is "static" like java
+  def max(x: Int, y: Int): Int = {
+    if (x > y) {
+      x;
+    }
+
+    y;
+  }
+
   def main(args: Array[String]) {
     println("Hello, World!")
+    println(max(3, 5))
   }
 }
