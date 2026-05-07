b97f6fb296a76a01d66a0548eea7968ff6e810c9
JimmyChin946 <Judes946@gmail.com>
2026-05-07 00:52:40 -0700

    comparing record

--- labs/bonus/hadoop/Record.java
+++ labs/bonus/hadoop/Record.java
@@ -4,39 +4,39 @@ import org.apache.hadoop.mapreduce.*;
 import org.apache.hadoop.mapreduce.Mapper.*;
 
 public class Record implements Comparable <Record> {
-	// An element of the inital input dataset
-	private int id;
-	private String name;
-	private double price;
+	private String month;
+	private String storeName;
+	private int storeId;
+	private String storeCity;
+	private double totalSales;
 
-	public Record(int id, String name, double price){
-		this.id = id;
-		this.name = name;
-		this.price = price;
+	public Record(String month, String storeName, int storeId, String storeCity, double totalSales){
+		this.month = month;
+		this.storeName = storeName;
+		this.storeId = storeId;
+		this.storeCity = storeCity;
+		this.totalSales = totalSales;
 	}
 
+	// Called when printint the final result
 	@Override
 	public String toString(){
-		return id+", "+name+", "+price;
+		return "(" + storeName + ", " + storeCity + ", " + totalSales + ")";
 	}
 	
 	// The price should be compared to in decending order
 	@Override
 	public int compareTo(Record other){
-		if(this.price > other.price) {
-			return -1;
-		} 
-
-		if(this.price < other.price){
-			return 1;
-		}
+		// First, we care about the sorting order by price. 
+		if(this.price > other.price) return -1;
+		if(this.price < other.price) return 1;
 	
 		// Breaks any ties between the records that have the same id
 		if (this.id > other.id) return  1 ;
 		if (this.id < other.id) return  -1 ;
 
 		// I belive that the ids are unique, however if it is not, we could do one 
-		// more comparison to distinguish the name as well
+		// more comparison to distinguish the names or the cities as well
 	
 		return 0;		// The records are the same
 	}
