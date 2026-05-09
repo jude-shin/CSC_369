d419a78a99ceb43918089bd4fcfa34fadfca9a30
JimmyChin946 <Judes946@gmail.com>
2026-05-09 10:13:30 -0700

    setup for asgn04

---
+++ assignments/asgn04/src/main/scala/example/App.scala
@@ -0,0 +1,20 @@
+package example
+object App {
+  def q1() = {
+
+  }
+
+  def q2() = {
+
+  }
+
+  def q3() = {
+
+  }
+
+  def main(args: Array[String]) {
+    println(q1())
+    // println(q2())
+    // println(q3())
+  }
+}
