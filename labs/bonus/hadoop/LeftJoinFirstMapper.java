ddf46458e13ffe33c1008cbda29d81a1c6f9afa7
JimmyChin946 <Judes946@gmail.com>
2026-05-11 15:35:10 -0700

    temporary secondary reducer

---
+++ labs/bonus/hadoop/LeftJoinFirstMapper.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.Mapper.*;
+
+public class LeftJoinFirstMapper 
+	extends Mapper<LongWritable, Text, PairOfStrings, PairOfStrings> {
+
+	@Override
+	public void map(LongWritable key, Text value, Context context)
+		throws IOException, InterruptedException {
+		
+		// The full line from the input file
+		String line = value.toString().trim();
+
+		// Split based on a comma
+		String[] tokens = line.split(",");
+
+		// Parse out the date, storeId, productId, quantity
+		String date = tokens[0].trim();
+		String storeId = tokens[1].trim();
+		String productId = tokens[2].trim();
+		String quantity = tokens[3].trim();
+	
+		// The order doesn't matter... I believe it is a one to one relationship, 
+		// where there is only one line item for one sales item, and vice versa
+		// So we will just secondary group and sort this first 
+		// ((productId, "2",) , ("date, storeId, quantity", "first"))
+
+		// pair of string b/c I am lazy... It will still secondary stort correct
+		PairOfStrings k = new PairOfStrings(new Text(salesId), new Text("1"));	
+
+		String valueLeft = date + ", " + storeId + ", " + productId;
+		PairOfStrings v = new PairOfStrings(new Text(valueLeft), new Text("first"));
+
+		context.write(k, v); 
+	}
+}
---
+++ labs/bonus/hadoop/SecondReducer.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import org.apache.log4j.Logger;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.Reducer.*;
+
+public class SecondReducer extends Reducer<PairOfStrings, PairOfStrings, NullWritable, Text> {
+
+	@Override
+	public void reduce(PairOfStrings key, Iterable<PairOfStrings> values, Context context) 
+		throws IOException, InterruptedException {
+
+		int i = 0;
+		for (PairOfStrings value : values) {
+			// // check for the first item
+			// if (i == 0 && "sale".equals(value.getRightElement().toString())) {
+			// 	saleInfo = value.getLeftElement().toString();
+			// }
+	
+			// // check for the second item
+			// if (i == 1 && "lineItem".equals(value.getRightElement().toString())) {
+			// 	lineItemInfo = value.getLeftElement().toString();
+			// 	break;
+			// }
+	
+			// // increment the counter
+			// i++;
+
+			Text out = new Text(
+					key.getLeftElement() + ", " +
+					key.getRightElement() + ", " + 
+					value.getLeftElement + ", " +
+					value.getRightElement()
+					);
+			context.write(NullWritable.get(), out);
+		}
+
+		// // Concatinate all of the data together!
+		// if (saleInfo != null && lineItemInfo != null) {
+		// 	// The output will be in the form (date, storeId, productId, quantity)
+		// 	Text out = new Text(saleInfo + ", " + lineItemInfo);
+		// 	context.write(NullWritable.get(), out);
+		// }
+	}
+}
