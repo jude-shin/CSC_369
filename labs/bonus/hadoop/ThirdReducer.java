5098cde5f1b08f48d5f522ef215257a8b3b715da
JimmyChin946 <Judes946@gmail.com>
2026-05-11 16:48:59 -0700

    only sending over what we need in the third reduce

--- labs/bonus/hadoop/ThirdReducer.java
+++ labs/bonus/hadoop/ThirdReducer.java
@@ -10,46 +10,30 @@ public class ThirdReducer extends Reducer<PairOfStrings, PairOfStrings, NullWrit
 	public void reduce(PairOfStrings key, Iterable<PairOfStrings> values, Context context) 
 		throws IOException, InterruptedException {
 
+		String nameCity = "";
+
 		int i = 0;
 		for (PairOfStrings value : values) {
-			//// check for the first item to be a product
-			//if (i == 0) {
-			//	if ("product".equals(value.getRightElement().toString())) {
-			//		descriptionPrice = value.getLeftElement().toString();
-			//	}
-			//	else {
-			//		// Don't write anything if there is no product before it
-			//		return;
-			//	}
-			//}
+			// check for the first item to be a product
+			if (i == 0) {
+				if ("store".equals(value.getRightElement().toString())) {
+					nameCity = value.getLeftElement().toString();
+				}
+				else {
+					// Don't write anything if there is no product before it
+					return;
+				}
+			}
 	
-			//// check for the other items to be of a diff
-			//if (i > 0 && "first".equals(value.getRightElement().toString())) {
-			//	//Text out = new Text(
-			//	//		key.getLeftElement() + ", " +
-			//	//		key.getRightElement() + ", " + 
-			//	//		value.getLeftElement() + ", " +
-			//	//		value.getRightElement()
-			//	//		);
-
-			//	// (description, price, date, storeId, quantity)
-			//	Text out = new Text(descriptionPrice + ", " + value.getLeftElement());
-			//	context.write(NullWritable.get(), out);
-			//}
+			// check for the other items to be of a diff type
+			if (i > 0 && "second".equals(value.getRightElement().toString())) {
+				// (storeId, name, city, price, date, quantity)
+				Text out = new Text(key.getLeftElement() + ", " + nameCity + ", " + value.getLeftElement());
+				context.write(NullWritable.get(), out);
+			}
 	
 			// increment the counter
 			i++;
-
-			Text out = new Text(
-					key.getLeftElement() + ", " +
-					key.getRightElement() + ", " + 
-					value.getLeftElement() + ", " +
-					value.getRightElement()
-					);
-	
-			// ((storeId, "1",) , ("storeName, city", "store"))
-			// ((storeId, "2",) , ("price, date, quantity", "second"))
-			context.write(NullWritable.get(), out);
 		}
 	}
 }
