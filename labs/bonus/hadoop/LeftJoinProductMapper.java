f3f5fa1d45b91c2dd55d00c6be3412b9ff5d8963
JimmyChin946 <Judes946@gmail.com>
2026-05-11 15:35:30 -0700

    product mapper for the second join

---
+++ labs/bonus/hadoop/LeftJoinProductMapper.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.Mapper.*;
+
+public class LeftJoinProductMapper 
+	extends Mapper<LongWritable, Text, PairOfStrings, PairOfStrings> {
+
+	@Override
+	public void map(LongWritable key, Text value, Context context)
+		throws IOException, InterruptedException {
+		
+		// The full line from the input file
+		String line = value.toString().trim();
+
+		// Split based on a comma
+		String[] tokens = line.split(",");
+
+		// Parse out the productId, description, price
+		String productId = tokens[0].trim();
+		String description = tokens[1].trim();
+		String price = tokens[2].trim();
+	
+		// The order doesn't matter... I believe it is a one to one relationship, 
+		// where there is only one line item for one sales item, and vice versa
+		// So we will just secondary group and sort this second
+		// ((productId, "2",) , ("description, price", "product"))
+
+		// pair of string b/c I am lazy... It will still secondary stort correct
+		PairOfStrings k = new PairOfStrings(new Text(salesId), new Text("2"));	
+
+		String valueLeft = description + ", " + price;
+		PairOfStrings v = new PairOfStrings(new Text(valueLeft), new Text("product"));
+
+		context.write(k, v); 
+	}
+}
