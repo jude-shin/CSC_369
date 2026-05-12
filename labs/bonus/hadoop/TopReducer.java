fdbd60b6721d4fc36b95b583a6f65d8274f4a42a
JimmyChin946 <Judes946@gmail.com>
2026-05-11 17:04:00 -0700

    reducer for top n solution

--- labs/bonus/hadoop/TopReducer.java
+++ labs/bonus/hadoop/TopReducer.java
@@ -6,7 +6,7 @@ import org.apache.hadoop.mapreduce.Reducer.*;
 import java.util.TreeSet;
 
 public class TopReducer extends Reducer<PairOfStrings, PairOfStrings, NullWritable, Text> {
-
+	private int n = MyDriver.DEFAULT_N;
 	private TreeSet<Record> top = new TreeSet<>();
 
 	@Override
@@ -32,11 +32,11 @@ public class TopReducer extends Reducer<PairOfStrings, PairOfStrings, NullWritab
 			// Add the element to the sorted running set
 			top.add(new Record(id, name, city, total));
 
+			// Keep only top n
+			if (top.size() > n) {
+				top.remove(top.last());
+			}
 
-			// // Keep only top n
-			// if (top.size() > n) {
-			// 	top.remove(top.last());
-			// }
 			// Text out = new Text(
 			// 		key.getLeftElement() + ", " +
 			// 		key.getRightElement() + ", " + 
@@ -46,11 +46,35 @@ public class TopReducer extends Reducer<PairOfStrings, PairOfStrings, NullWritab
 
 			// context.write(NullWritable.get(), out);
 
-			// Finally, write the global top n records as the output
+			// Write the yearMonth first
+			String ouput = key.getLeftElement().toString() + ", ";
+			
+			// Continue to write the 
+			int i = 1;
+			int stop = top.size();
 			for(Record r : top){
 				// Will be in the same form as the input ("id, name, price")
-				context.write(NullWritable.get(), new Text(r.toString()));
+				output += r.toString();
+
+				if (i != stop) {
+					output += ", ";
+				}
 			}
+	
+			// Write it all out!
+			context.write(NullWritable.get(), new Text(output));
+		}
+	}
+
+	// Run before the reduce stage
+	@Override
+	protected void setup(Context context) throws IOException, InterruptedException {
+		// Either sets up the global variable from the given context, or the default
+		this.n = context.getConfiguration().getInt("N", MyDriver.DEFAULT_N); 
+
+		// If for some reason the top is not initalized, initalize it to an empty set
+		if (this.top == null) {
+			this.top = new TreeSet<Record>();
 		}
 	}
 }
