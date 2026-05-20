7d05ac3b8b2e1a4f5e8fa0b7427ad4f25d2aa944
JimmyChin946 <Judes946@gmail.com>
2026-05-20 16:19:45 -0700

    testing

--- assignments/asgn04/src/main/scala/example/App.scala
+++ assignments/asgn04/src/main/scala/example/App.scala
@@ -6,7 +6,10 @@ object App {
   def q1() = {
     val intsPath = "inputs/q1"
     val ints: List[String] = Source.fromFile(intsPath).getLines.toList
-    ints.flatMap(_.split(" ").toList).filter(x => x.asInstanceOf[Int]%3 == 0).foreach(println)
+    ints
+      .flatMap(_.split(" ").toList)
+      // .filter(x => x.asInstanceOf[Int]%3 == 0)
+      .foreach(println)
   }
 
   def q2() = {
