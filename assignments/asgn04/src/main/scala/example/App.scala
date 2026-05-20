4cd53d1ff57b8a244e56a0ab2bd4d69275b10102
JimmyChin946 <Judes946@gmail.com>
2026-05-20 16:21:15 -0700

    I don't know why this doesn't work

--- assignments/asgn04/src/main/scala/example/App.scala
+++ assignments/asgn04/src/main/scala/example/App.scala
@@ -7,7 +7,7 @@ object App {
     val intsPath = "inputs/q1"
     val ints: List[String] = Source.fromFile(intsPath).getLines.toList
     ints
-      .flatMap(_.split(" ").toList)
+      // .flatMap(_.split(" ").toList)
       // .filter(x => x.asInstanceOf[Int]%3 == 0)
       .foreach(println)
   }
