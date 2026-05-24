4297bfcc19af82937d30bca2fb2b63fd9301513a
JimmyChin946 <Judes946@gmail.com>
2026-05-24 11:16:55 -0700

    we should just try it out

--- assignments/asgn05/src/main/scala/example/App.scala
+++ assignments/asgn05/src/main/scala/example/App.scala
@@ -11,33 +11,30 @@ import scala.collection._
 object App {
 
   def q1(sc: SparkContext) = {
-    // Raw input lines from a text file
+    // Line: int int int ...
     val integersRdd = sc.textFile("input/asgn05/q1_integers/")
-
-    // Each integer is parsed individually
-    var rdd = integersRdd.flatMap(l => l.split(" ")).map(_.toInt)
-  
-    // Filter out all those who are divisible by 3
-    rdd = rdd.filter(_%3 == 0)
-  
-    // Creates a map of the element and it's frequency; prints the result
-    rdd.countByValue().foreach({
-      case (int, ct) => println(s"$int appears $ct times")
-      case _ => throw new IllegalArgumentException("You are the problem... You should never be here!")
-    })
+      .flatMap(l => l.split(" "))   // Space delimited
+      .map(_.toInt)                 // Convert each to an int
+      .filter(_%3 == 0)             // Only divisible by 3 allowed
+      .countByValue().foreach({     // Print every value and its frequency
+        case (int, ct) => println(s"$int appears $ct times")
+        case _ => throw new IllegalArgumentException("You are the problem... You should never be here!")
+      })
   }
 
   def q2(sc: SparkContext) = {
+    // Line: employee name, department id
     val employeeRdd = sc.textFile("input/asgn05/q2_employees/")
       .map(l => l.split(","))   // one array per line Array[String, String]
       .map({                    // trim the result and turn into a tuple
         case Array(s1, s2) => (s1.trim, s2.trim)
         case _ => throw new IllegalArgumentException("You are the problem... You should never be here!")
       })
-
+    
+    // Line: department id, department name
     val departmentRdd = sc.textFile("input/asgn05/q2_departments/")
       .map(l => l.split(","))   // one array per line Array[String, String]
-      .map({                    // trim the result and turn into a tuple
+      .map({                    // trim the result and convert into a tuple
         case Array(s1, s2) => (s1.trim, s2.trim)
         case _ => throw new IllegalArgumentException("You are the problem... You should never be here!")
       })
@@ -63,55 +60,52 @@ object App {
       })
   }
 
-  // def q3(sc: SparkContext) = {
-  //   // Raw input lines from a text file
-  //   val students = sc.textFile("input/asgn05/q3_students/")
-  // 
-  //   // Parse each line
-  //   // result in the format of (name, id, gradeCourses)
-  //   var rdd = students.map(l => l.split(",", 3))
+  def q3(sc: SparkContext) = {
+    // Raw input lines from a text file
+    val studentsRdd = sc.textFile("input/asgn05/q3_students/")
+      .map(l => l.split(",", 3))    // Parse to (name, id, gradeCourses)
+      .map({                        // Convert into a tuple
+        case Array(name, id, gradeCourse) => (name, id, gradeCourse)
+        case _ => throw new IllegalArgumentException("You are the problem... You should never be here!")
+      })
+      .map({                        // Extract the letter grades and get gpa
+        // Split every grade based on the comma
+        // Extract the grade letter for every course (the first char of the grade course pair)
+        // Calculate the gpa based on those letters
+        // result in the format of (name, id, grades)
+        case (name, id, gradeCourses) => 
+          (name, id, getGpa(gradeCourses.split(",").map(_.trim).map(_.substring(0, 1))))
+        case _ => throw new IllegalArgumentException("You are the problem... You should never be here!")
+      })
+      .collect().foreach({          // Print the result
+      // TODO: why is this an array?
+      case Array(name, id, gpa) =>
+        println(s"$name, $id, $gpa")
+    })
+  }
+  
+  // Returns the average GPA from a list of letter grades
+  def getGpa(gs: String*): Double = {
+    // Maps all the letters to a numerical value, then sums, then divides by the 
+    // number of strings that were given
+    return gs.map(x => lToNGrade(x)).fold(0.0)({(x, y) => x+y}) / gs.length
+  }
 
-  //   // Parse out only the letter grade from the list of students
-  //   rdd = rdd.map({
-  //     // Split every grade based on the comma
-  //     // Extract the grade letter for every course (the first character of the grade course pair)
-  //     // Calculate the gpa based on those letters
-  //     // result in the format of (name, id, grades)
-  //     case Array(name, id, gradeCourses) => 
-  //       (name, id, getGpa(gradeCourses.split(",").map(_.trim).map(_.substring(0, 1))))
-  //     case _ => throw new IllegalArgumentException("You are the problem... You should never be here!")
-  //   })
-
-  //   // Print the result
-  //   rdd.collect().foreach({
-  //     // TODO: why is this an array?
-  //     case Array(name, id, gpa) =>
-  //       println(s"$name, $id, $gpa")
-  //   })
-  // }
-  // 
-  // // Returns the average GPA from a list of letter grades
-  // def getGpa(gs: String*): Double = {
-  //   // Maps all the letters to a numerical value, then sums, then divides by the 
-  //   // number of strings that were given
-  //   return gs.map(x => lToNGrade(x)).fold(0.0)({(x, y) => x+y}) / gs.length
-  // }
-
-  // def lToNGrade(l: String): Double = {
-  //   return l match {
-  //     case "A" => 4
-  //     case "a" => 4
-  //     case "B" => 3
-  //     case "b" => 3
-  //     case "C" => 2
-  //     case "c" => 2
-  //     case "D" => 1
-  //     case "d" => 1
-  //     case "F" => 0
-  //     case "f" => 0
-  //     case _ => Double.NaN
-  //   }
-  // }
+  def lToNGrade(l: String): Double = {
+    return l match {
+      case "A" => 4
+      case "a" => 4
+      case "B" => 3
+      case "b" => 3
+      case "C" => 2
+      case "c" => 2
+      case "D" => 1
+      case "d" => 1
+      case "F" => 0
+      case "f" => 0
+      case _ => Double.NaN
+    }
+  }
 
   def main(args: Array[String]) {
     // Don't log a bunch of the junk
