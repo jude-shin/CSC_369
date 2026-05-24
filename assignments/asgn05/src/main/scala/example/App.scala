73c949bd536edd609a23609a119fcbf52338e5aa
JimmyChin946 <Judes946@gmail.com>
2026-05-24 10:10:21 -0700

    testing question 2

--- assignments/asgn05/src/main/scala/example/App.scala
+++ assignments/asgn05/src/main/scala/example/App.scala
@@ -27,86 +27,86 @@ object App {
     })
   }
 
-  // def q2(sc: SparkContext) = {
-  //   // Raw input lines from a text file
-  //   var employeeRdd = sc.textFile("input/asgn05/q2_employees")
-  //   var departmentRdd = sc.textFile("input/asgn05/q2_departments")
-
-  //   // Parse the inputs to tuples (String, String)
-  //   employeeRdd = employeeRdd.flatMap(l => l.split(",").map(_.trim))
-  //   departmentRdd = departmentRdd.flatMap(l => l.split(",").map(_.trim))
-
-  //   // Cartesian product of the two inputs
-  //   // result in the format ((ename, did), (did, dname))
-  //   var rdd = employeeRdd.cartesian(departmentRdd)
-  // 
-  //   // Filter those who only have the same did (basically a join on did)
-  //   rdd = rdd.filter({
-  //     // ((ename, did), (did2, dname))
-  //     // empl._2 and dept._1 is the did
-  //     case (empl, dept) => empl._2 == dept._1
-  //     case _ => throw new IllegalArgumentException("You are the problem... You should never be here!")
-  //   })
-  // 
-  //   // Creates a list of all those joined; prints the output
-  //   rdd.collect().foreach({
-  //     // ((ename, did), (did2, dname))
-  //     // empl._1 is the employee name
-  //     // dept._2 is the department name
-  //     case (empl, dept) => println(s"$empl._1, $dept._2")
-  //     case _ => throw new IllegalArgumentException("You are the problem... You should never be here!")
-  //   })
-  // }
-
-  def q3(sc: SparkContext) = {
+  def q2(sc: SparkContext) = {
     // Raw input lines from a text file
-    val students = sc.textFile("input/asgn05/q3_students")
-  
-    // Parse each line
-    // result in the format of (name, id, gradeCourses)
-    var rdd = students.map(l => l.split(",", 3))
+    var employeeRdd = sc.textFile("input/asgn05/q2_employees")
+    var departmentRdd = sc.textFile("input/asgn05/q2_departments")
 
-    // Parse out only the letter grade from the list of students
-    rdd = rdd.map({
-      // Split every grade based on the comma
-      // Extract the grade letter for every course (the first character of the grade course pair)
-      // Calculate the gpa based on those letters
-      // result in the format of (name, id, grades)
-      case Array(name, id, gradeCourses) => 
-        (name, id, getGpa(gradeCourses.split(",").map(_.trim).map(_.substring(0, 1))))
+    // Parse the inputs to tuples (String, String)
+    employeeRdd = employeeRdd.flatMap(l => l.split(",").map(_.trim))
+    departmentRdd = departmentRdd.flatMap(l => l.split(",").map(_.trim))
+
+    // Cartesian product of the two inputs
+    // result in the format ((ename, did), (did, dname))
+    var rdd = employeeRdd.cartesian(departmentRdd)
+  
+    // Filter those who only have the same did (basically a join on did)
+    rdd = rdd.filter({
+      // ((ename, did), (did2, dname))
+      // empl._2 and dept._1 is the did
+      case (empl, dept) => empl._2 == dept._1
       case _ => throw new IllegalArgumentException("You are the problem... You should never be here!")
     })
-
-    // Print the result
+  
+    // Creates a list of all those joined; prints the output
     rdd.collect().foreach({
-      // TODO: why is this an array?
-      case Array(name, id, gpa) =>
-        println(s"$name, $id, $gpa")
+      // ((ename, did), (did2, dname))
+      // empl._1 is the employee name
+      // dept._2 is the department name
+      case (empl, dept) => println(s"$empl._1, $dept._2")
+      case _ => throw new IllegalArgumentException("You are the problem... You should never be here!")
     })
   }
-  
-  // Returns the average GPA from a list of letter grades
-  def getGpa(gs: String*): Double = {
-    // Maps all the letters to a numerical value, then sums, then divides by the 
-    // number of strings that were given
-    return gs.map(x => lToNGrade(x)).fold(0.0)({(x, y) => x+y}) / gs.length
-  }
 
-  def lToNGrade(l: String): Double = {
-    return l match {
-      case "A" => 4
-      case "a" => 4
-      case "B" => 3
-      case "b" => 3
-      case "C" => 2
-      case "c" => 2
-      case "D" => 1
-      case "d" => 1
-      case "F" => 0
-      case "f" => 0
-      case _ => Double.NaN
-    }
-  }
+  // def q3(sc: SparkContext) = {
+  //   // Raw input lines from a text file
+  //   val students = sc.textFile("input/asgn05/q3_students")
+  // 
+  //   // Parse each line
+  //   // result in the format of (name, id, gradeCourses)
+  //   var rdd = students.map(l => l.split(",", 3))
+
+  //   // Parse out only the letter grade from the list of students
+  //   rdd = rdd.map({
+  //     // Split every grade based on the comma
+  //     // Extract the grade letter for every course (the first character of the grade course pair)
+  //     // Calculate the gpa based on those letters
+  //     // result in the format of (name, id, grades)
+  //     case Array(name, id, gradeCourses) => 
+  //       (name, id, getGpa(gradeCourses.split(",").map(_.trim).map(_.substring(0, 1))))
+  //     case _ => throw new IllegalArgumentException("You are the problem... You should never be here!")
+  //   })
+
+  //   // Print the result
+  //   rdd.collect().foreach({
+  //     // TODO: why is this an array?
+  //     case Array(name, id, gpa) =>
+  //       println(s"$name, $id, $gpa")
+  //   })
+  // }
+  // 
+  // // Returns the average GPA from a list of letter grades
+  // def getGpa(gs: String*): Double = {
+  //   // Maps all the letters to a numerical value, then sums, then divides by the 
+  //   // number of strings that were given
+  //   return gs.map(x => lToNGrade(x)).fold(0.0)({(x, y) => x+y}) / gs.length
+  // }
+
+  // def lToNGrade(l: String): Double = {
+  //   return l match {
+  //     case "A" => 4
+  //     case "a" => 4
+  //     case "B" => 3
+  //     case "b" => 3
+  //     case "C" => 2
+  //     case "c" => 2
+  //     case "D" => 1
+  //     case "d" => 1
+  //     case "F" => 0
+  //     case "f" => 0
+  //     case _ => Double.NaN
+  //   }
+  // }
 
   def main(args: Array[String]) {
     // Don't log a bunch of the junk
@@ -119,7 +119,7 @@ object App {
     
     // =========================================================================
     // q1(sc)
-    // q2(sc)
-    q3(sc)
+    q2(sc)
+    // q3(sc)
   }
 }
