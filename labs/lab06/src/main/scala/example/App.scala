22ca9710b13f49e317c30302c778633766651d39
JimmyChin946 <Judes946@gmail.com>
2026-05-23 20:12:35 -0700

    RDDs

--- labs/lab06/src/main/scala/example/App.scala
+++ labs/lab06/src/main/scala/example/App.scala
@@ -36,7 +36,6 @@ object App {
     val job3 =
       for {
         (storeId, lineTotal) <- job2
-        Store(sId, _, _, _, _, state, _) <- stores
         if storeId == sId
       } yield Record(storeId, state, lineTotal)
 
@@ -45,44 +44,85 @@ object App {
 
 
   def main(args: Array[String]) {
-    // PARSE INPUTS //
-    // Note that this has to be in the project directory
-    val lineItemPath = "inputs/lineItem"
-    val salePath = "inputs/sale"
-    val productPath = "inputs/product"
-    val storePath = "inputs/store"
+    // Don't log a bunch of the junk
+    Logger.getLogger("org").setLevel(Level.OFF)
+    Logger.getLogger("akka").setLevel(Level.OFF)
 
-    // Read the text from the source
-    // Each line is parsed into a dataclass of the given type (comma delimeted)
-    val lineItems: List[LineItem] = Source.fromFile(lineItemPath).getLines.toList.map(LineItem.parse)
-    val sales: List[Sale] = Source.fromFile(salePath).getLines.toList.map(Sale.parse)
-    val products: List[Product] = Source.fromFile(productPath).getLines.toList.map(Product.parse)
-    val stores: List[Store] = Source.fromFile(storePath).getLines.toList.map(Store.parse)
+    // Spark setup
+    val conf = new SparkConf().setAppName("NameOfApp")
+    val sc = new SparkContext(conf)
 
-    // =========================================================================
+    // Parse input files into RDDs
+    val lineItemPath = "/user/jshin53/input/lineItem"
+    val salePath = "/user/jshin53/input/sale"
+    val productPath = "/user/jshin53/input/product"
+    val storePath = "/user/jshin53/input/store"
 
-    // JOIN ITEMS //
-    var records: List[Record]= Record.joinall(lineItems)(sales)(products)(stores)
-
-    // GROUP BY ID//
-    var storeToRecords: Map[Int, List[Record]] = records.groupBy(_.getId)
-
-    // AGGREGATE //
-    // each store now has a total sum
-    var storeTotal: List[Record] = storeToRecords.map { 
-      case (id, rs) =>
-        Record(id, rs.head.getState, rs.foldLeft(0.0)((total, r) => total + r.getSales))
-    }.toList
+    val lineItems = sc.textFile(lineItemPath)
+    val sales = sc.textFile.(salePath).getLines
+    val products = sc.textFile.(productPath)
+    val stores = sc.textFile(storePath)
 
     // =========================================================================
-    // Note that at this point we have a list of Records with their total sales
-    // There should only be one record for one store
 
-    // SORT //
-    // Sort everything, first on state, then on total sale, finally by id
-    val sorted: List[Record] = storeTotal.sortBy(r => (r.getState, r.getSales, r.getId))
+    // JOINS
 
-    // PRINT //
-    sorted.foreach(println)
+    // job1: join the lineItems and the sales on the saleId 
+    // structure: (productId, (saleId, quantity, storeId))
+    val lineItemTuple = lineItems.map({
+      case LineItem(_, saleId, productId, quantity) => (saleId, (productId, quantity))
+    })
+    val saleTuple = sales.map({
+      case Sale(saleId, _, _, storeId, _) => (saleId, storeId)
+    })
+    val job1Tuple = lineItemTuple.join(saleTuple).map({
+      case (saleId, ((productId, quantity), storeId)) => 
+        (productId, (saleId, quantity, storeId))
+    })
+
+    // =========================================================================k
+
+    // job2: join job1 and the products on the productId
+    // structure: (storeId, (saleId, productId, quantity, price))
+    val productTuple = products.map({
+      case Product(productId, _, price) => (productId, price)
+    })
+    val job2 = job1Tuple.join(productTuple).map({
+      case (productId, ((saleId, quantity, storeId), price)) => 
+        (storeId, (saleId, productId, quantity, price))
+    })
+
+    // =========================================================================k
+    // job3: join job2 and the store on the storeId
+    // structure: (saleId, productId, quantity, storeId, price, state)
+    val storeTuple = products.map({
+      case Store(storeId, _, _, _, _, state, _) => (storeId, state)
+    })
+    val job3 = job2Tuple.join(storeTuple).map({
+      case (productId, ((saleId, productId, quantity, price), state)) => 
+        (saleId, productId, quantity, storeId, price, state)
+    })
+
+
+    // // GROUP BY ID//
+    // var storeToRecords: Map[Int, List[Record]] = records.groupBy(_.getId)
+
+    // // AGGREGATE //
+    // // each store now has a total sum
+    // var storeTotal: List[Record] = storeToRecords.map { 
+    //   case (id, rs) =>
+    //     Record(id, rs.head.getState, rs.foldLeft(0.0)((total, r) => total + r.getSales))
+    // }.toList
+
+    // // =========================================================================
+    // // Note that at this point we have a list of Records with their total sales
+    // // There should only be one record for one store
+
+    // // SORT //
+    // // Sort everything, first on state, then on total sale, finally by id
+    // val sorted: List[Record] = storeTotal.sortBy(r => (r.getState, r.getSales, r.getId))
+
+    // // PRINT //
+    job3.collect().foreach(println)
   }
 }
