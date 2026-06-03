f299545e8161c525054b9f91cef5f6a88f32a3c3
JimmyChin946 <Judes946@gmail.com>
2026-06-02 22:26:26 -0700

    reduce by key is a tuple of tuples

--- assignments/asgn06/src/main/scala/example/App.scala
+++ assignments/asgn06/src/main/scala/example/App.scala
@@ -147,7 +147,7 @@ object App {
     
     // Get average gpa for students who have taken courses
     val averageGrade = takenRdd
-      .reduceByKey((gr, ct) => (gr._1+gr._2, ct._1+ct._2))
+      .reduceByKey((x, y) => (x._1+y._1, x._2+y._2))
       .mapValues({ case (gr, ct) => gr/ct})
 
     // Left join, turning None into 0.0 average gpa
